8154e9ed0bbda22b36686e3bd866232d5d9f25af
NicolasOfficial <pablosanchez3318@gmail.com>
2023-11-07 17:41:44 -0600

    lkjn

--- Proyectopainicial.php
+++ Proyectopainicial.php
@@ -178,7 +178,7 @@ crossorigin="anonymous"></script>
 
         <img src="<?php  echo $imagen;  ?>" width="300" width="300">
 
-      </div>'´'
+      </div>
     </div>
        <hr class="featurette-divider">
 
